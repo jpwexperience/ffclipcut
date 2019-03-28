dd70c00dc409a4090e37cc3677bc994f1d1b083d
John <darumswizel@aol.com>
2019-03-27 20:12:15 -0500

    Adding some functions to simplify the code

--- ffshell.sh
+++ ffshell.sh
@@ -1,16 +1,94 @@
 #!/usr/bin/env bash
+#Does accept multiple inputs but they are run sequentially
+#total streams
+streamArr=()
+
+videoArr=()
+videoMetaArr=()
+videoTypeArr=()
+videoChoice=0 #user input video stream choice
+
+audioArr=()
+audioMetaArr=()
+audioTypeArr=()
+audioChoice=0 #user input subtitle stream choice
+
+subtitleArr=()	
+subtitleTypeArr=()
+subtitleMetaArr=()
+subtitleChoice=0 #user input subtitle stream choice
+
+ffprobeOut=() #ffprobe output array. each element is the next line
+verboseFl=0 #full ffprobe output
+
+inputSize=0 #number of ffprobe output lines 
+
+#$1: User Input 
+#$2: Number of Streams
 inRangeCode=0
 in_range () {
 	compare=$(($2-1))
 	echo "$1 and $compare"
 	if (( $1 > $compare )) || (( $1 < -1)); then
-		inRange=0
+		#echo "nah man"
+		inRangeCode=0
+	elif (( $1 == -1)); then
+		inRangeCode=-1
 	else
-		inRange=1
+		#echo "ayy there it is"
+		inRangeCode=$1
 	fi
 }
 
-verboseFl=0
+if [ 1 -eq 0 ]; then
+#$1: Type of Stream
+#$2: User Input
+stream_check () {
+	sType=$1
+	if [[ sType == "audio" ]]; then
+		sLen=${#audioArr[@]}
+		sChoice=0
+		sTypeArr=$videoTypeArr
+	elif [[ sType == "subtitle" ]]; then
+		sLen=${#subtitleArr[@]}	
+		sChoice=0
+		sTypeArr=$videoTypeArr
+	else [[ sType == "video" ]]
+		sLen=${#videoArr[@]}
+		sChoice=0
+		sTypeArr=$videoTypeArr
+	fi
+	if (( ${#videoArr[@]} == 0 )); then
+		echo "No Video Tracks"
+		videoChoice=-1
+	elif (( ${#videoArr[@]} == 1 )); then
+		#echo "One Video"
+		videoChoice=0
+		echo "${videoArr[videoChoice]}"
+	else
+		count=0
+		echo -e "\n\t-Video Streams-"
+		for i in "${videoArr[@]}"; do
+			firstChunk=${i%%,*}
+			mainContent=${firstChunk##*:\ }
+			#echo "$firstChunk"
+			echo "$count) | $firstChunk"
+			type=${mainContent##*:}
+			type=${type#\ }
+			type=${type%%\ *}
+			echo "$type"
+			videoTypeArr+=("$type")
+			count=$((count+1))
+		done
+		echo "Type the number corresponding to the video stream you want to use, followed by [ENTER]:"
+		read videoChoice
+		in_range $videoChoice ${#videoArr[@]} 
+		echo "$inRangeCode"
+		videoChoice=inRangeCode
+	fi
+}
+	fi
+
 #set flags for input
 for i in "$@"; do
 	if [[ $i == "-v" ]]; then
@@ -35,14 +113,8 @@ for i in "$@"; do
 				echo "$i"
 			done
 		fi
-		echo -e "\n${#info[@]}"
+		#echo -e "\n${#info[@]}"
 		IFS='\n' read -ra ADDR <<< "$info"
-		count=0
-		for i in "${ADDR[@]}"; do
-			echo "$count"
-			count=$((count+1))
-		done
-		echo -e "\n${#arrIN[@]}"
 		streamArr=()
 		videoArr=()
 		videoMetaArr=()
@@ -55,16 +127,10 @@ for i in "$@"; do
 		subtitleMetaArr=()
 		ffprobeOut=()
 
-		#$1: Stream Type
-		#$2: 
-		#stream_rip () {
-		
-		#}
-
-		echo "wat"
 		#read each line of ffprobe output
 		#for line in "${ffprobeOut[@]}"; do
 		while read -r line; do
+			ffprobeOut+=("$line")
 			#echo "... $line ..."
 			if [[ $line =~ (S|s)"tream"(.*) ]]; then
 				#echo "$line"	
@@ -73,49 +139,16 @@ for i in "$@"; do
 					videoArr+=("$line")
 					#this is causing issues if no metadata is available
 					#Possibly skips next stream
-					read line2
-					if [[ $line2 =~  (.*)(M|m)"etadata"(.*) ]]; then
-						#echo "metadata"
-						read line3
-						if [[ $line3 =~  (.*)(T|t)"itle"(.*) ]]; then
-							#echo "$line3"
-							videoMetaArr+=("$line3")
-						fi
-					else
-						videoMetaArr+=("null")
-					fi
 				elif [[ $line =~ (.*)(A|a)"udio"(.*) ]]; then
 				       	audioArr+=("$line")
-					read line2
-					if [[ $line2 =~  (.*)(M|m)"etadata"(.*) ]]; then
-						#echo "metadata"
-						read line3
-						if [[ $line3 =~  (.*)(T|t)"itle"(.*) ]]; then
-							#echo "$line3"
-							audioMetaArr+=("$line3")
-						fi
-					else
-						audioMetaArr+=("null")
-					fi
 				elif [[ $line =~ (.*)(S|s)"ubtitle"(.*) ]]; then
 					subtitleArr+=("$line")
-					read line2
-					if [[ $line2 =~  (.*)(M|m)"etadata"(.*) ]]; then
-						#echo "metadata"
-						read line3
-						if [[ $line3 =~  (.*)(T|t)"title"(.*) ]]; then
-							#echo "$line3"
-							subtitleMetaArr+=("$line3")
-						fi
-					else
-						subtitleMetaArr+=("null")
-					fi
 				else
 					echo "$line"
 					echo "Unidentified Stream"
 				fi	
 
-			fi	
+			fi
 		done <<< "$info"
 		#print out all streams
 
@@ -123,6 +156,12 @@ for i in "$@"; do
 		echo "Number of video streams: ${#videoArr[@]} | Number of audio streams: ${#audioArr[@]} | Number of subtitle streams: ${#subtitleArr[@]}"
 		
 		
+		echo "${#ffprobeOut[@]}"
+		for i in "${ffprobeOut[@]}"; do
+			echo -e "\n"
+		done
+		yooo=0
+		echo "${ffprobeOut[yooo]}"
 		echo -e "\n\n---------------------------------"
 		#echo "number of stream pieces: $temp"
 		#for i in ${streamArr[@]}; do
@@ -163,9 +202,9 @@ for i in "$@"; do
 		else
 			count=0
 			echo -e "\n\t-Audio Streams-"
-			for i in $(seq 1 ${#audioArr[@]}); do
-				echo $i
-			done
+			#for i in $(seq 1 ${#audioArr[@]}); do
+			#	echo $i
+			#done
 			for i in "${audioArr[@]}"; do
 				firstChunk=${i%%,*}
 				mainContent=${firstChunk##*:\ }
